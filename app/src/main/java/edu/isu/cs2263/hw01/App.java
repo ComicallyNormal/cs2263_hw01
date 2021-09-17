f9b79b9d9b10f784502322e674c6eb339ca3f310
Alex <dandiviney@isu.edu>
2021-09-16 20:20:49 -0600

    .gitignore is more fixed

--- app/src/main/java/edu/isu/cs2263/hw01/App.java
+++ app/src/main/java/edu/isu/cs2263/hw01/App.java
@@ -50,9 +50,9 @@ public class App {
 
     public static void main(String[] args) {
 
-        for (String arg : args) {
-            System.out.println("Got argument [" + arg + "]");
-        }
+//        for (String arg : args) {
+//            System.out.println("Got argument [" + arg + "]");
+//        }
         //The command line parser will let us parse our options list.
         CommandLineParser clp = new DefaultParser();
 
